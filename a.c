5467ae9e287b79748ae78731ac367e858d044dfa
Никола <mn080202@gmail.com>
2015-08-02 13:23:53 +0000

    Update a.c

--- a.c
+++ a.c
@@ -1,3 +1,32 @@
-int main(){
-	printf("asd");
+#include "stdlib.h"
+#define width 5
+#define height 5
+#define size width*height
+char x[size] = {0,1,0,0,0,0
+               ,1,0,0,2,0,0
+               ,1,0,0,2,0,0
+               ,1,0,0,2,0,0
+               ,1,0,0,2,0,0};
+
+int cur;
+
+int f(char c ){
+  char r=x[cur*width+c];
+  return r;
+}
+
+int main(int argc,void * argv){
+
+  int i=0;
+  int r=0;
+  char *xx="asdasd";
+  while (i<strlen(xx)){
+    printf("strlen %d i=%d\n",strlen(xx),i);
+    r=f(x[i]-'a');
+    if (r>0) break
+    printf("r=%d",r);
+    i++;
+  }
+  printf("x");
+  return 0;
 }
